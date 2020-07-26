02717321b384b27b1b2b62b660f9a60eb8a1cb90
Sachin Khanal <sackhanaal@gmail.com>
2020-07-26 16:08:13 -0500

    Firebase Login and Register created

--- Message Me/Controller/LoginViewController.swift
+++ Message Me/Controller/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -18,8 +19,23 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     @IBAction func loginButtonIsPressed(_ sender: UIButton) {
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+              guard let strongSelf = self else { return }
+                
+                if let e = error {
+                    print(e)
+                }
+                else{
+                    self!.performSegue(withIdentifier: "loginbutton", sender: self)
+                }
+              // ...
+            }
+        }
         
-        self.performSegue(withIdentifier: "loginbutton", sender: self)
+        
+        
+        
     }
     /*
     // MARK: - Navigation
--- Message Me/Controller/RegisterViewController.swift
+++ Message Me/Controller/RegisterViewController.swift
@@ -7,25 +7,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func registerButtonIsPressed(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "registerbutton", sender: self)
+        if let email = emailTextField.text, let password = passwordTextField.text{
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                
+                if let e = error {
+                    print(e.localizedDescription)
+                }
+                else{
+                    self.performSegue(withIdentifier: "registerbutton", sender: self)
+                }
+                
+            }
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "registerbutton" {
-             let _ = segue.destination as! ChatViewController
-                
-               print("segue created")
+            let _ = segue.destination as! ChatViewController
+            
+            print("segue created")
             
         }
         else{
@@ -35,13 +47,13 @@ class RegisterViewController: UIViewController {
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
